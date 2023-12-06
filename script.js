462c096654284f1c80a1c13178ca184eea0a783b
Aditya verma <60606311+Adityavrmaa@users.noreply.github.com>
2023-12-06 21:19:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,68 +7,68 @@ let videoStream;
 
 disabledButtons(true);
 
-function startCam() {
-    // Check if the device is mobile
+async function startCam() {
     const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
     const constraints = {
-        video: {},
+        video: {
+            facingMode: isMobile ? "environment" : "user",
+        },
     };
 
-    if (isMobile) {
-        // For mobile devices, use the back camera
-        constraints.video.facingMode = { exact: "environment" };
-    } else {
-        // For desktop, allow user to choose a camera
-        constraints.video.facingMode = "user";
-    }
+    try {
+        videoStream = await navigator.mediaDevices.getUserMedia(constraints);
+        videoPreview.srcObject = videoStream;
+        disabledButtons(false);
 
-    navigator.mediaDevices
-        .getUserMedia(constraints)
-        .then(async (stream) => {
-            videoStream = stream;
-            videoPreview.srcObject = stream;
-            disabledButtons(false);
-
-            buttonScreenshot.addEventListener("click", async () =>
-                takeScreenshot(videoStream));
-
-            buttonDownload.addEventListener("click", async () =>
-                startDownload(videoStream));
-        })
-        .catch((error) => {
-            console.error('Error accessing webcam:', error);
-            disabledButtons(true);
-        });
-}
-
-function stopCam() {
-    if (videoStream) {
-        // Stop all tracks in the stream
-        videoStream.getTracks().forEach(track => track.stop());
-        videoPreview.srcObject = null; // Remove the video source
+        buttonScreenshot.addEventListener("click", () => takeScreenshot());
+        buttonDownload.addEventListener("click", () => startDownload());
+    } catch (error) {
+        console.error('Error accessing webcam:', error);
         disabledButtons(true);
     }
 }
 
-async function getImageBlob(videoStream) {
-    const track = videoStream.getVideoTracks()[0];
-    const imageBlob = await new ImageCapture(track).takePhoto();
-    return imageBlob;
+function stopCam() {
+    if (videoStream) {
+        videoStream.getTracks().forEach(track => track.stop());
+        videoPreview.srcObject = null;
+        disabledButtons(true);
+    }
 }
 
-async function takeScreenshot(videoStream) {
-    screenshot.src = URL.createObjectURL(await getImageBlob(videoStream));
+async function takeScreenshot() {
+    if (videoStream) {
+        const track = videoStream.getVideoTracks()[0];
+        const imageCapture = new ImageCapture(track);
+
+        try {
+            const imageBlob = await imageCapture.takePhoto();
+            screenshot.src = URL.createObjectURL(imageBlob);
+        } catch (error) {
+            console.error('Error taking photo:', error);
+        }
+    }
 }
 
-async function startDownload(videoStream) {
-    const imageURL = URL.createObjectURL(await getImageBlob(videoStream));
-    const link = document.createElement("a");
-    link.href = imageURL;
-    link.download = `screenshot-${new Date().toLocaleTimeString()}.jpg`;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+async function startDownload() {
+    if (videoStream) {
+        const track = videoStream.getVideoTracks()[0];
+        const imageCapture = new ImageCapture(track);
+
+        try {
+            const imageBlob = await imageCapture.takePhoto();
+            const imageURL = URL.createObjectURL(imageBlob);
+            const link = document.createElement("a");
+            link.href = imageURL;
+            link.download = `screenshot-${new Date().toLocaleTimeString()}.jpg`;
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        } catch (error) {
+            console.error('Error taking photo for download:', error);
+        }
+    }
 }
 
 function disabledButtons(state) {
@@ -76,8 +76,5 @@ function disabledButtons(state) {
     buttonScreenshot.disabled = state;
 }
 
-// Example: Call startCam when a button is clicked
 document.querySelector("#start-button").addEventListener("click", startCam);
-
-// Example: Call stopCam when a button is clicked
 document.querySelector("#stop-button").addEventListener("click", stopCam);
